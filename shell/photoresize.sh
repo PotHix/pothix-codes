d93a2ac3fe01800d8e0b245ef32d6ee5318d249f
PotHix <pothix@pothix.com>
2010-07-04 21:25:25 -0300

    A little script to resize my 10MB pictures to send by email :P

---
+++ shell/photoresize.sh
@@ -0,0 +1,30 @@
+DIR=converted
+CURRENT_DIR=`pwd`
+
+resize (){
+   FILES_FOLDER=$CURRENT_DIR/$1
+
+   if [ ! -d $2 ]
+   then
+      mkdir $2
+   fi
+
+   for i in `ls $FILES_FOLDER`
+   do
+      convert $FILES_FOLDER/$i -resize 1024x768 $2/$i
+   done
+}
+
+case $# in
+   1)
+      resize $1 $CURRENT_DIR/$1/$DIR
+      ;;
+   2)
+      resize $1 $2/$DIR
+      ;;
+   *)
+      echo "Wrong number of arguments";
+      echo "Use:";
+      echo "photoresize relative/path/to/pictures [relative/path/to/converted/pictures]";
+      ;;
+esac
