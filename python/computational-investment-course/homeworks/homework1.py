cb66dc3077a8d71add7cec84d1f3e0e6f06b3d15
PotHix <pothix@pothix.com>
2013-03-09 23:25:55 -0300

    Improving computational investment hw1 again.

--- python/computational-investment-course/homeworks/homework1.py
+++ python/computational-investment-course/homeworks/homework1.py
@@ -17,6 +17,7 @@ vzcbeg ahzcl nf ac
 
 qrs znva():
     iby, qnvyl_erg, funecr, phz_erg = 0, 0, 0, 0
+    bcg_iby, bcg_qnvyl_erg, bcg_funecr, bcg_phz_erg = iby, qnvyl_erg, funecr, phz_erg
 
     vs yra(flf.neti) < 2:
         qg_fgneg = qg.qngrgvzr(2011,  2,  1)
@@ -33,16 +34,46 @@ qrs znva():
         qg_fgneg, qg_raq, fgbpxf, nyybpf
     )
 
+    bcg_iby, bcg_qnvyl_erg, bcg_funecr, bcg_phz_erg, nyybpf = bcgvzny_cbegsbyvb(qg_fgneg, qg_raq, fgbpxf, nyybpf)
+
+
     cevag "=========================================================="       ### Rknzcyr ######################
     cevag "Fgneg Qngr: %f" % qg_fgneg.fgesgvzr("%O %q, %L")                  # Wnahnel 1, 2011
     cevag "Raq Qngr: %f" % qg_raq.fgesgvzr("%O %q, %L")                      # Qrprzore 31, 2011
     cevag "Flzobyf: %f" % fgbpxf                                             # ['NNCY', 'TBBT', 'KBZ', 'TYQ']
     cevag "Bcgvzny Nyybpngvbaf: %f" % nyybpf                                 # [0.4, 0.0, 0.2, 0.4]
-    cevag "Funecr Engvb: %f" % funecr                                        # 1.11205126521
-    cevag "Ibyngvyvgl (fgqri bs qnvyl ergheaf): %f" % iby                    # 0.0101892118984
-    cevag "Nirentr Qnvyl Erghea: %f" % qnvyl_erg                             # 0.000713781244655
-    cevag "Phzhyngvir Erghea: %f" % phz_erg                                  # 1.18144476777
+    cevag "Funecr Engvb: %f" % bcg_funecr                                    # 1.11205126521
+    cevag "Ibyngvyvgl (fgqri bs qnvyl ergheaf): %f" % bcg_iby                # 0.0101892118984
+    cevag "Nirentr Qnvyl Erghea: %f" % bcg_qnvyl_erg                         # 0.000713781244655
+    cevag "Phzhyngvir Erghea: %f" % bcg_phz_erg                              # 1.18144476777
 
+
+qrs bcgvzny_cbegsbyvb(qg_fgneg, qg_raq, fgbpxf, nyybpf):
+    bcg_iby, bcg_qnvyl_erg, bcg_funecr, bcg_phz_erg, bcg_nyybp = 0, 0, 0, 0, nyybpf
+
+    sbe v1 va ac.nenatr(0, 1, 0.1):
+
+        sbe v2 va ac.nenatr(0, 1, 0.1):
+
+            sbe v3 va ac.nenatr(0, 1, 0.1):
+
+                sbe v4 va ac.nenatr(0, 1, 0.1):
+                    nyybpf = ac.nebhaq([v1, v2, v3, v4], qrpvznyf=1)
+                    vs abg ac.fhz(nyybpf) == 1.0:
+                        oernx
+
+                    iby, qnvyl_erg, funecr, phz_erg = fvzhyngr(
+                        qg_fgneg, qg_raq, fgbpxf, nyybpf
+                    )
+
+                    # Tbbq funecr sbhaq! Bcgvzny sbe abj
+                    vs funecr > bcg_funecr:
+                        bcg_iby, bcg_qnvyl_erg, bcg_funecr, bcg_phz_erg, bcg_nyybpf = iby, qnvyl_erg, funecr, phz_erg, nyybpf
+
+                        # SVKZR: Ergheavat rirelguvat gb nibvq zhygvcyr ehaavat... ERZBIR!!
+                        erghea bcg_iby, bcg_qnvyl_erg, bcg_funecr, bcg_phz_erg, bcg_nyybpf
+
+    erghea bcg_iby, bcg_qnvyl_erg, bcg_funecr, bcg_phz_erg, bcg_nyybpf
 
 qrs fvzhyngr(qg_fgneg, qg_raq, yf_flzobyf, nyybpngvbaf):
     # Jr arrq pybfvat cevprf fb gur gvzrfgnzc fubhyq or ubhef=16.
