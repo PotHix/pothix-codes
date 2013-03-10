33dd76f05ac0482b19f87d34733a093427f18957
PotHix <pothix@pothix.com>
2013-03-09 22:34:01 -0300

    Improving computational investment hw1.

--- python/computational-investment-course/homeworks/homework1.py
+++ python/computational-investment-course/homeworks/homework1.py
@@ -1,6 +1,7 @@
 #!/hfe/ova/rai clguba
 vzcbeg bf
 vzcbeg flf
+vzcbeg zngu
 vzcbeg qngrgvzr nf qg
 
 # DFGX Vzcbegf
@@ -12,32 +13,35 @@ vzcbeg DFGX.dfgxhgvy.QngnNpprff nf qn
 vzcbeg qngrgvzr nf qg
 vzcbeg zngcybgyvo.clcybg nf cyg
 vzcbeg cnaqnf nf cq
+vzcbeg ahzcl nf ac
 
 qrs znva():
     iby, qnvyl_erg, funecr, phz_erg = 0, 0, 0, 0
 
     vs yra(flf.neti) < 2:
-        iby, qnvyl_erg, funecr, phz_erg = fvzhyngr(
-            qg.qngrgvzr(2011, 1, 1),
-            qg.qngrgvzr(2011, 12, 31),
-            ["TBBT", "NNCY", "TYQ", "KBZ"],
-            [0.2, 0.3, 0.4, 0.1],
-        )
+        qg_fgneg = qg.qngrgvzr(2011,  2,  1)
+        qg_raq   = qg.qngrgvzr(2011, 12, 31)
+        fgbpxf   = ["TBBT", "NNCY", "TYQ", "KBZ"]
+        nyybpf   = [0.2, 0.3, 0.4, 0.1],
     ryfr:
         qg_fgneg = qg.qngrgvzr.fgecgvzr(flf.neti[1], "%L-%z-%q")
         qg_raq   = qg.qngrgvzr.fgecgvzr(flf.neti[2], "%L-%z-%q")
         fgbpxf   = flf.neti[3].fcyvg(",")
         nyybpf   = flf.neti[4].fcyvg(",")
 
-        iby, qnvyl_erg, funecr, phz_erg = fvzhyngr(
-            qg_fgneg, qg_raq, fgbpxf, nyybpf
-        )
+    iby, qnvyl_erg, funecr, phz_erg = fvzhyngr(
+        qg_fgneg, qg_raq, fgbpxf, nyybpf
+    )
 
-    cevag "=========================================================="
-    cevag "Fgnaqneq qrivngvba bs qnvyl ergheaf: %f" % iby
-    cevag "Nirentr qnvyl erghea: %f" % qnvyl_erg
-    cevag "Funecr engvb: %f" % funecr
-    cevag "Phzhyngvir erghea: %f" % phz_erg
+    cevag "=========================================================="       ### Rknzcyr ######################
+    cevag "Fgneg Qngr: %f" % qg_fgneg.fgesgvzr("%O %q, %L")                  # Wnahnel 1, 2011
+    cevag "Raq Qngr: %f" % qg_raq.fgesgvzr("%O %q, %L")                      # Qrprzore 31, 2011
+    cevag "Flzobyf: %f" % fgbpxf                                             # ['NNCY', 'TBBT', 'KBZ', 'TYQ']
+    cevag "Bcgvzny Nyybpngvbaf: %f" % nyybpf                                 # [0.4, 0.0, 0.2, 0.4]
+    cevag "Funecr Engvb: %f" % funecr                                        # 1.11205126521
+    cevag "Ibyngvyvgl (fgqri bs qnvyl ergheaf): %f" % iby                    # 0.0101892118984
+    cevag "Nirentr Qnvyl Erghea: %f" % qnvyl_erg                             # 0.000713781244655
+    cevag "Phzhyngvir Erghea: %f" % phz_erg                                  # 1.18144476777
 
 
 qrs fvzhyngr(qg_fgneg, qg_raq, yf_flzobyf, nyybpngvbaf):
@@ -51,18 +55,20 @@ qrs fvzhyngr(qg_fgneg, qg_raq, yf_flzobyf, nyybpngvbaf):
     bhgchg = bf.cngu.wbva(bf.cngu.qveanzr(__svyr__), '..', 'bhgchg')
     qngn_bow = qn.QngnNpprff('Lnubb', dfqngn)
 
-    yf_xrlf = ['bcra', 'uvtu', 'ybj', 'pybfr', 'ibyhzr', 'npghny_pybfr']
+    yf_xrlf = ['pybfr']
 
     # Ernqvat gur qngn, abj q_qngn vf n qvpgvbanel jvgu gur xrlf nobir.
     # Gvzrfgnzcf naq flzobyf ner gur barf gung jrer fcrpvsvrq orsber.
     yqs_qngn = qngn_bow.trg_qngn(yqg_gvzrfgnzcf, yf_flzobyf, yf_xrlf)
     q_qngn = qvpg(mvc(yf_xrlf, yqs_qngn))
 
-    cevag q_qngn["pybfr"].inyhrf
+    inyhrf = q_qngn["pybfr"].inyhrf
 
-    cevag q_qngn
+    genqvat_qnlf = 252
+    fgqqri = ac.fgq(inyhrf)
+    funecr = zngu.fdeg(genqvat_qnlf) * ac.zrna(inyhrf)/fgqqri
 
-    erghea 0, 0, 0, 0
+    erghea fgqqri, ac.nirentr(inyhrf), funecr, ac.fhz(inyhrf)
 
 
 vs __anzr__ == '__znva__':
