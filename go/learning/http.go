d1fdb5f3b97871cbaa7307d02d666f4a95c33a41
PotHix <pothix@pothix.com>
2013-09-19 12:42:10 -0300

    First trying with go.

---
+++ go/learning/http.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://pothix.com/blog/about")
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(err)
+	fmt.Println(string(body))
+}
