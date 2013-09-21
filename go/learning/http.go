407341a1a98eaeb44b6ba28d1a3fda11b297a60e
PotHix <pothix@pothix.com>
2013-09-21 11:18:14 -0300

    Added the simple go server.

--- go/learning/http.go
+++ go/learning/http.go
@@ -1,3 +1,5 @@
+// +build http
+
 package main
 
 import (
---
+++ go/learning/http_server.go
@@ -0,0 +1,21 @@
+// +build http_server
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+    fmt.Fprint(w, "Hello PotHix!")
+}
+
+func main() {
+    var h Hello
+    http.ListenAndServe("localhost:4000", h)
+}
