3e9994d747059ff93e28d1eb597b048d69051fb5
PotHix <pothix@pothix.com>
2013-10-02 10:49:15 -0300

    Finished lab 1

--- r/coursera-statistics-one/basics.R
+++ r/coursera-statistics-one/basics.R
@@ -30,3 +30,22 @@ rbind(x,y)
 
 # -----------------------------------------------------------------------
 # Dataframes (a matrix to construct different types of objects
+d=data.frame(id=1:5,gender=c("M","F","F","M","F"),score=c(8,3,6,5,5))
+
+nrol(d) # number of rows
+ncol(d) # number of columns
+
+attributes(d) # check the attributes of the data frame as we do with matrix
+
+d[2,1] # get the second row first column value from data frame
+
+View(d) # Open a TCL windows for dataframe viewing
+edit(d) # Open a TCL windows for dataframe editting
+
+?edit # get help for the edit function
+
+install.packages("psych") # install psych library
+
+library(psych) # Load psych library
+
+search() # show loaded libraries
