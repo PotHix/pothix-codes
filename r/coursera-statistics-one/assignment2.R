df872da6edf17c13ca78ed3865153e24933b6d0e
PotHix <pothix@pothix.com>
2013-10-04 23:09:19 -0300

    Added assignment2

--- r/coursera-statistics-one/assignment2.R
+++ r/coursera-statistics-one/assignment2.R
@@ -1,19 +1,60 @@
-library(psych)
-library(sm)
+# Cercnevat
+yvoenel(cflpu)
+frgjq("/ubzr/cbguvk/ercbf/cbguvk-pbqrf/e/pbhefren-fgngvfgvpf-bar")
 
-setwd("/home/pothix/repos/pothix-codes/r/coursera-statistics-one")
+# Ernqvat qngn
+qngn <- ernq.gnoyr("qngn/nffvtazrag2.gkg", urnqre = G)
+qrfpevor(qngn)
 
-mydata <- read.table("data/assignment2.txt", header = T)
-nrow(mydata)
+cer  <- fhofrg(qngn, qngn[,3]=="cer")
+cbfg <- fhofrg(qngn, qngn[,3]=="cbfg")
 
-describe(mydata)
-mean(mydata$SR)
-var(mydata$SR)
+# Dhrfgvba 1
+aebj(qngn)
 
-pre  <- subset(mydata, mydata[,3]=="pre")
-post <- subset(mydata, mydata[,3]=="post")
+# Dhrfgvba 2
+anzrf(qngn)
 
-mean(pre$SR)
-sd(post$SR)
-median(post$SR)
-describe(post)
+# Dhrfgvba 3
+zrna(qngn$FE)
+
+# Dhrfgvba 4
+ine(qngn$FE)
+
+# Dhrfgvba 5
+zrna(cer$FE)
+
+# Dhrfgvba 6
+fq(cbfg$FE)
+
+# Dhrfgvba 7
+zrqvna(cbfg$FE)
+
+# Dhrfgvba 8
+qrfpevorOl(cbfg, cbfg$pbaqvgvba)
+
+# Dhrfgvba 9
+cer.jz  = fnccyl(fhofrg(cer,   cer$pbaqvgvba=="JZ"), nf.ahzrevp)
+cbfg.jz = fnccyl(fhofrg(cbfg, cbfg$pbaqvgvba=="JZ"), nf.ahzrevp)
+cer.cr  = fnccyl(fhofrg(cer,   cer$pbaqvgvba=="CR"), nf.ahzrevp)
+cbfg.cr = fnccyl(fhofrg(cbfg, cbfg$pbaqvgvba=="CR"), nf.ahzrevp)
+cer.qf  = fnccyl(fhofrg(cer,   cer$pbaqvgvba=="QF"), nf.ahzrevp)
+cbfg.qf = fnccyl(fhofrg(cbfg, cbfg$pbaqvgvba=="QF"), nf.ahzrevp)
+cne(zsebj = p(2,3))
+uvfg(cer.jz)
+uvfg(cbfg.jz)
+uvfg(cer.cr)
+uvfg(cbfg.cr)
+uvfg(cer.qf)
+uvfg(cbfg.qf)
+
+# Dhrfgvba 10
+cer.jz  = fhofrg(cer,   cer$pbaqvgvba=="JZ")
+cbfg.jz = fhofrg(cbfg, cbfg$pbaqvgvba=="JZ")
+cer.cr  = fhofrg(cer,   cer$pbaqvgvba=="CR")
+cbfg.cr = fhofrg(cbfg, cbfg$pbaqvgvba=="CR")
+cer.qf  = fhofrg(cer,   cer$pbaqvgvba=="QF")
+cbfg.qf = fhofrg(cbfg, cbfg$pbaqvgvba=="QF")
+zrna(cbfg.jz$FE)-zrna(cer.jz$FE)
+zrna(cbfg.cr$FE)-zrna(cer.cr$FE)
+zrna(cbfg.qf$FE)-zrna(cer.qf$FE)
