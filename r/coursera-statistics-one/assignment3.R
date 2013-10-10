6fed5fe3db3c0fee1895cc0b14433c5a04067a3b
PotHix <pothix@pothix.com>
2013-10-10 09:41:11 -0300

    Preparing data for assignment 3.

---
+++ r/coursera-statistics-one/assignment3.R
@@ -0,0 +1,30 @@
+# Preparing
+library(psych)
+setwd("/home/pothix/repos/pothix-codes/r/coursera-statistics-one")
+
+# Reading data
+data <- read.table("data/assignment3.txt", header = T)
+describe(data)
+
+pre  <- subset(data, data[,3]=="pre")
+post <- subset(data, data[,3]=="post")
+
+# Question 1
+
+# Question 2
+
+# Question 3
+
+# Question 4
+
+# Question 5
+
+# Question 6
+
+# Question 7
+
+# Question 8
+
+# Question 9
+
+# Question 10
