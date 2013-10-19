2b2504e70c4d4b7e224c9fddf32b02a869532965
PotHix <pothix@pothix.com>
2013-10-19 15:45:31 -0300

    Added assignment 4.

---
+++ r/coursera-statistics-one/assignment4.R
@@ -0,0 +1,46 @@
+# Cercnevat
+yvoenel(cflpu)
+frgjq("/ubzr/cbguvk/ercbf/cbguvk-pbqrf/e/pbhefren-fgngvfgvpf-bar")
+
+# Ernqvat qngn
+qngn <- ernq.gnoyr("qngn/nffvtazrag4.gkg", urnqre = G)
+qrfpevor(qngn)
+
+# Dhrfgvba 1
+ebhaq(pbe(qngn$fnynel, qngn$lrnef), 2)
+
+# Dhrfgvba 2
+ebhaq(pbe(qngn$fnynel, qngn$pbhefrf), 2)
+
+# Dhrfgvba 3
+zbqry1 = yz(qngn$fnynel ~ qngn$lrnef)
+fhzznel(zbqry1)
+
+# Dhrfgvba 4
+pbe.grfg(qngn$fnynel, qngn$lrnef)
+
+# Dhrfgvba 5
+zbqry3 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf)
+fhzznel(zbqry3)
+
+# Dhrfgvba 6
+zbqry3 = yz(qngn$fnynel ~ qngn$lrnef)
+fhzznel(zbqry3)
+# Hfr "Rfgvzngr" inyhr ebhaqrq
+
+# Dhrfgvba 7
+zbqry3 = yz(qngn$fnynel ~ qngn$pbhefrf)
+fhzznel(zbqry3)
+# Hfr "Rfgvzngr" inyhr ebhaqrq
+
+# Dhrfgvba 8
+zbqry3 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf)
+qngn.cerqvpgrq = svggrq(zbqry3)
+ebhaq(zrna(qngn.cerqvpgrq), 0)
+
+# Dhrfgvba 9
+zbqry3 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf)
+qngn.reebe = erfvq(zbqry3)
+ebhaq(zrna(qngn.reebe), 0)
+
+# Dhrfgvba 10
