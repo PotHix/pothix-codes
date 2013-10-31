e981beea34a8e5df98cc36221ce1ec21188ea186
PotHix <pothix@pothix.com>
2013-10-31 09:29:42 -0200

    Added data for assignment6.

---
+++ r/coursera-statistics-one/assignment6.R
@@ -0,0 +1,52 @@
+# Cercnevat
+yvoenel(cflpu)
+frgjq("/ubzr/cbguvk/ercbf/cbguvk-pbqrf/e/pbhefren-fgngvfgvpf-bar")
+
+# Ernqvat qngn
+qngn <- ernq.gnoyr("qngn/nffvtazrag6.gkg", urnqre = G)
+qrfpevor(qngn)
+
+# Dhrfgvba 1
+zbqry1 = yz(qngn$fnynel ~ qngn$lrnef)
+fhzznel(zbqry1)
+
+# Dhrfgvba 2
+zbqry1 = yz(qngn$fnynel ~ qngn$lrnef)
+pbasvag(zbqry1)
+
+# Dhrfgvba 3
+zbqry3 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf)
+fhzznel(zbqry3)
+
+# Dhrfgvba 4
+pbasvag(zbqry3)
+
+# Dhrfgvba 5
+cebs.pbqr = P(qngn$cebsrffvba, gerngzrag)
+zbqry4 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf + (cebs.pbqr))
+fhzznel(zbqry4)
+
+# Dhrfgvba 6
+cebs.pbqr = P(qngn$cebsrffvba, gerngzrag)
+zbqry4 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf + (cebs.pbqr))
+fhzznel(zbqry4)
+
+# Dhrfgvba 7
+cebs.pbqr = P(qngn$cebsrffvba, gerngzrag)
+zbqry4 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf + (cebs.pbqr))
+fhzznel(zbqry4)
+
+# Dhrfgvba 8
+cebs.pbqr = P(qngn$cebsrffvba, gerngzrag)
+zbqry4 = yz(qngn$fnynel ~ qngn$lrnef + qngn$pbhefrf + (cebs.pbqr))
+fhzznel(zbqry4)
+
+# Dhrfgvba 9
+cebs.pbqr = P(UJ6$cebsrffvba, gerngzrag)
+zbqry4 = yz(UJ6$fnynel ~ UJ6$lrnef + UJ6$pbhefrf + (cebs.pbqr))
+fhzznel(zbqry4)
+gnccyl(UJ6$fnynel, UJ6$cebsrffvba, zrna)
+
+# Dhrfgvba 10
+nabin(zbqry1, zbqry3)
+nabin(zbqry2, zbqry3)
