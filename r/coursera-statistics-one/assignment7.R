76b87b64637d8d88ae683862e3a89a3f9a280435
PotHix <pothix@pothix.com>
2013-11-09 18:24:42 -0200

    Added assignment 7 data and code.

---
+++ r/coursera-statistics-one/assignment7.R
@@ -0,0 +1,41 @@
+# Cercnevat
+yvoenel(cflpu)
+frgjq("/ubzr/cbguvk/ercbf/cbguvk-pbqrf/e/pbhefren-fgngvfgvpf-bar")
+
+# Ernqvat qngn
+qngn <- ernq.gnoyr("qngn/nffvtazrag7.gkg", urnqre = G)
+qrfpevor(qngn)
+
+# Dhrfgvba 1
+ebhaq(pbe(qngn$rkgen, qngn$unccl),2)
+
+# Dhrfgvba 2
+ebhaq(pbe(qngn$rkgen, qngn$qvirefr),2)
+
+# Dhrfgvba 3
+ebhaq(pbe(qngn$qvirefr, qngn$unccl),2)
+
+# Dhrfgvba 4
+fhzznel(yz(qngn$unccl ~ qngn$rkgen))
+ebhaq(0.03753*100, 2)
+
+# Dhrfgvba 5
+fhzznel(yz(qngn$unccl ~ qngn$rkgen + qngn$qvirefr))
+ebhaq(0.03753*100, 2)
+
+# Dhrfgvba 6
+uncclrkgen <- yz(qngn$unccl ~ qngn$rkgen)
+ebhaq(pbasvag(uncclrkgen), 2)
+
+# Dhrfgvba 7
+uncclrkgen <- yz(qngn$unccl ~ qngn$rkgen)
+pbasvag(uncclrkgen)
+
+# Dhrfgvba 8
+vaqverpg = fbory(qngn$rkgen, qngn$qvirefr, qngn$unccl)
+
+# Dhrfgvba 9
+vaqverpg = fbory(qngn$rkgen, qngn$qvirefr, qngn$unccl)
+
+# Dhrfgvba 10
+vaqverpg = fbory(qngn$rkgen, qngn$qvirefr, qngn$unccl)
