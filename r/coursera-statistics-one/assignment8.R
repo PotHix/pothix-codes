652ed74092fac9fddc6d02ab2e34f274bf8f8c0d
PotHix <pothix@pothix.com>
2013-11-15 09:52:09 -0200

    Added initial files for lab8 and assignment8.

---
+++ r/coursera-statistics-one/assignment8.R
@@ -0,0 +1,27 @@
+# Preparing
+library(psych)
+setwd("/home/pothix/repos/pothix-codes/r/coursera-statistics-one")
+
+# Reading data
+data <- read.table("data/assignment8.txt", header = T)
+describe(data)
+
+# Question 1
+
+# Question 2
+
+# Question 3
+
+# Question 4
+
+# Question 5
+
+# Question 6
+
+# Question 7
+
+# Question 8
+
+# Question 9
+
+# Question 10
