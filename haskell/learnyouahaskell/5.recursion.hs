3da9dd8dd03af8a2ac22bd96fe35f29e909defe9
PotHix <pothix@pothix.com>
2014-02-11 20:00:38 -0200

    More haskell study notes.

---
+++ haskell/learnyouahaskell/5.recursion.hs
@@ -0,0 +1,63 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+-- or even better
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum' xs)
+
+
+-- recursive replicate
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+
+-- redefining take
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
+
+-- reverse
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+
+-- infinite list with a new implementation of repeat
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+
+-- recursive zip
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+
+-- recursive implementation of `elem` function
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
+
+
+-- quicksort
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
