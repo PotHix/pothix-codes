7eff53c44e5cc1f6546193d6300226d4a97c01d9
PotHix <pothix@pothix.com>
2015-10-17 23:33:17 -0300

    Added exercise3 from fp101

---
+++ haskell/functional-programming-101-edx/exercise3.hs
@@ -0,0 +1,20 @@
+mylist = [1,2,10]
+myinit = [1,2]
+
+init1 xs = tail $ reverse xs
+init2 xs = reverse $ head $ reverse xs
+init3 xs = reverse $ tail xs
+init4 xs = take (length xs) xs
+init5 xs = reverse $ tail $ reverse xs
+init6 xs = take (length xs - 1) (tail xs)
+init7 xs = drop (length xs - 1) xs
+
+main :: IO ()
+main = do
+  putStrLn $ show $ init1 mylist == myinit
+  putStrLn $ show False -- wrong type when comparing
+  putStrLn $ show $ init3 mylist == myinit
+  putStrLn $ show $ init4 mylist == myinit
+  putStrLn $ show $ init5 mylist == myinit
+  putStrLn $ show $ init6 mylist == myinit
+  putStrLn $ show $ init7 mylist == myinit
