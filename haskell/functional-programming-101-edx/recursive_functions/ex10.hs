5b72de6cdb72fad9f4150e6d6d2784b0e297d857
PotHix <pothix@pothix.com>
2015-11-04 10:51:49 -0200

    Code for ex10 of fp101 recursion class

---
+++ haskell/functional-programming-101-edx/recursive_functions/ex10.hs
@@ -0,0 +1,14 @@
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x : xs) (y : ys)
+  = if x <= y then x : merge xs (y : ys) else y : merge (x : xs) ys
+
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+msort :: Ord a => [a] -> [a]
+msort []  = []
+msort [x] = [x]
+msort xs = merge (msort ys) (msort zs)
+           where (ys, zs) = halve xs
