0cfee545c988a2c9a0dfa6be4cb00d5c35eadd3b
PotHix <pothix@pothix.com>
2021-04-07 01:03:41 -0300

    Improve the code a bit more
    I still don't know why `.day()` doesn't work for DateTime<Local>.

--- rust/twitter-export-to-roam/src/main.rs
+++ rust/twitter-export-to-roam/src/main.rs
@@ -1,23 +1,28 @@
 use serde::{Deserialize, Serialize};
 use std::error::Error;
-use std::fs;
 use std::io;
 use std::process;
 
+use chrono::prelude::DateTime;
+use chrono::Local;
+use std::time::{Duration, UNIX_EPOCH};
+
 #[derive(Debug, Deserialize)]
 struct Tweet {
-    timestamp: String,
+    timestamp: u64,
     tweet: String,
 }
 
 #[derive(Serialize)]
 struct RoamDaily {
+    // must be on the lines of "April 1st, 2021"
     title: String,
     children: Vec<RoamLine>,
 }
 
 #[derive(Serialize)]
 struct RoamLine {
+    // must follow the structure: "12:13 Working on my stuff #tweet"
     string: String,
 }
 
@@ -26,25 +31,35 @@ fn transform() -> Result<(), Box<dyn Error>> {
     for result in reader.deserialize() {
         let tweet: Tweet = result?;
 
+        let d = UNIX_EPOCH + Duration::from_secs(tweet.timestamp);
+        let datetime = DateTime::<Local>::from(d);
+
+        let strftime_string = "%B %dth %Y";
+        /*
+        let strftime_string = match datetime.day() {
+            1 => "%B %dst, %Y",
+            2 => "%B %dnd, %Y",
+            3 => "%B %drd, %Y",
+            21 => "%B %dst, %Y",
+            22 => "%B %dnd, %Y",
+            23 => "%B %drd, %Y",
+            31 => "%B %dst, %Y",
+            _ => "%B %dth %Y",
+        };
+        */
+
         let mut lines = Vec::new();
         lines.push(RoamLine {
             string: tweet.tweet.clone(),
         });
 
         let daily = RoamDaily {
-            title: tweet.timestamp.clone(),
+            title: datetime.format(strftime_string).to_string(), //tweet.timestamp.clone(),
             children: lines,
         };
 
-        let mut filename = "/tmp/".to_string();
-        filename.push_str("April 4th, 2001");
-        filename.push_str(".md");
-
         let content = serde_json::to_string(&daily).unwrap();
         println!("{}", content);
-
-        fs::write(filename.as_str().to_lowercase(), content.as_bytes())
-            .expect("could not write to the markdown file");
     }
     Ok(())
 }
